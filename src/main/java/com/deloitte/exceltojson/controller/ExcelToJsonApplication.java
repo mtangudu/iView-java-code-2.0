37a1039843502f85733d151baddda8f64c49f5a7
mtangudu <33445497+mtangudu@users.noreply.github.com>
2020-05-27 16:44:15 +0530

    Update ExcelToJsonApplication.java

--- src/main/java/com/deloitte/exceltojson/controller/ExcelToJsonApplication.java
+++ src/main/java/com/deloitte/exceltojson/controller/ExcelToJsonApplication.java
@@ -2,9 +2,11 @@ package com.deloitte.exceltojson.controller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 public class ExcelToJsonApplication  {
 	
 	public static void main(String args[]) {
